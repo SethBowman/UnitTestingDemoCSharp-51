54129498d2e228644f6aff282d548e5674adba98
Seth Bowman <brandonsethbowman@gmail.com>
2024-12-19 16:40:21 -0600

    demo completed

---
+++ MainProject/Helper.cs
@@ -0,0 +1,19 @@
+﻿namespace MainProject;
+
+public class Helper
+{
+    public int Add(int numOne, int numTwo)
+    {
+        return numOne + numTwo;
+    }
+
+    public string Greet(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            return "Hello!";
+        }
+        
+        return $"Hello, {name}!";
+    }
+}
---
+++ UnitTests/HelperTests.cs
@@ -0,0 +1,39 @@
+﻿using MainProject;
+
+namespace UnitTests;
+
+public class HelperTests
+{
+    [Theory]
+    [InlineData(2, 2, 4)]
+    [InlineData(5, 5, 10)]
+    public void AddTest(int numOne, int numTwo, int expected)
+    {
+        //Arrange
+        var helper = new Helper();
+
+        //Act
+        var actual = helper.Add(numOne, numTwo);
+
+        //Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("Seth", "Hello, Seth!")]
+    [InlineData("Tyson", "Hello, Tyson!")]
+    [InlineData("Miah", "Hello, Miah!")]
+    [InlineData(null, "Hello!")]
+    [InlineData("", "Hello!")]
+    public void GreetTest(string name, string expected)
+    {
+        //Arrange
+        var helper = new Helper();
+        
+        //Act
+        var actual = helper.Greet(name);
+        
+        //Assert
+        Assert.Equal(expected, actual);
+    }
+}
